220552de2bd91533d69c0309d37802f75c5b88e1
Md Tasmim Al Tahsin <105342016+tasmimtahsin02@users.noreply.github.com>
2023-02-01 00:47:35 +0600

    Add files via upload

---
+++ User.java
@@ -0,0 +1,20 @@
+
+public class User {
+	
+	String username;
+	String password;
+	
+	User(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getpassword() {
+		return this.password;
+	}
+
+}
